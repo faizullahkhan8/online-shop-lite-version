0d2931bed2ae679b09b09d07cb11949b8e82e60b
Faiz Ullah Khan <faizullahofficial0@gmail.com>
2026-02-07 16:42:52 +0500

    feat: Initialize full-stack application with Express backend, Axios API client, and Vercel deployment configurations.

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: [process.env.FRONTEND_URL,"http://localhost:5173"],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
         credentials: true,
     })
